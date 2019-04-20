3f8822f9e7e0f6681945854992c3615e36850273
Chris Sanders <sanders.chris@gmail.com>
2019-04-20 10:32:19 -0500

    Sync with latest template functional testing

--- tests/functional/test_deploy.py
+++ tests/functional/test_deploy.py
@@ -1,9 +1,8 @@
 import os
 import pytest
 import subprocess
-# from juju.model import Model
 
-# Treat tests as coroutines
+# Treat all tests as coroutines
 pytestmark = pytest.mark.asyncio
 
 juju_repository = os.getenv('JUJU_REPOSITORY', '.').rstrip('/')
@@ -12,10 +11,21 @@ series = ['xenial',
           pytest.param('cosmic', marks=pytest.mark.xfail(reason='canary')),
           ]
 sources = [('local', '{}/builds/duplicati'.format(juju_repository)),
-           ('jujucharms', 'cs:~chris.sanders/duplicati'),
+           # ('jujucharms', 'cs:...'),
            ]
 
 
+# Uncomment for re-using the current model, useful for debugging functional tests
+# @pytest.fixture(scope='module')
+# async def model():
+#     from juju.model import Model
+#     model = Model()
+#     await model.connect_current()
+#     yield model
+#     await model.disconnect()
+
+
+# Custom fixtures
 @pytest.fixture(params=series)
 def series(request):
     return request.param
@@ -26,34 +36,22 @@ def source(request):
     return request.param
 
 
-# @pytest.fixture
-# async def model():
-#     model = Model()
-#     await model.connect_current()
-#     yield model
-#     await model.disconnect()
-
-
-# @pytest.fixture(params=series)
 @pytest.fixture
 async def app(model, series, source):
-    return model.applications['duplicati-{}-{}'.format(series, source[0])]
+    app_name = 'duplicati-{}-{}'.format(series, source[0])
+    return await model._wait_for_new('application', app_name)
 
 
-async def test_duplicate_deploy(model, series, source):
-    # Starts a deploy for each series and source
-    subprocess.check_call(['juju',
-                           'deploy',
-                           source[1],
-                           '-m', model.info.name,
-                           'duplicati-{}-{}'.format(series, source[0]),
-                           ])
-
-    # await model.deploy(source[1],
-    #                    series=series,
-    #                    application_name='duplicati-{}-{}'.format(series,
-    #                                                              source[0],
-    #                                                              ))
+async def test_duplicati_deploy(model, series, source, request):
+    # Starts a deploy for each series
+    # Using subprocess b/c libjuju fails with JAAS
+    # https://github.com/juju/python-libjuju/issues/221
+    application_name = 'duplicati-{}-{}'.format(series, source[0])
+    cmd = ['juju', 'deploy', source[1], '-m', model.info.name,
+           '--series', series, application_name]
+    if request.node.get_closest_marker('xfail'):
+        cmd.append('--force')
+    subprocess.check_call(cmd)
 
 
 async def test_haproxy_deploy(model):
@@ -65,24 +63,23 @@ async def test_haproxy_deploy(model):
 async def test_charm_upgrade(model, app):
     if app.name.endswith('local'):
         pytest.skip("No need to upgrade the local deploy")
-    # await model.block_until(lambda: app.status == 'active')
     unit = app.units[0]
     await model.block_until(lambda: unit.agent_status == 'idle')
-    # await app.upgrade_charm(switch=sources[0][1]) # Not Implemented yet
     subprocess.check_call(['juju',
                            'upgrade-charm',
                            '--switch={}'.format(sources[0][1]),
                            '-m', model.info.name,
                            app.name,
                            ])
-    unit = app.units[0]
     await model.block_until(lambda: unit.agent_status == 'executing')
-    await model.block_until(lambda: unit.agent_status == 'idle')
 
 
-async def test_duplicati_status(app, model):
+# Tests
+async def test_duplicati_status(model, app):
     # Verifies status for all deployed series of the charm
     await model.block_until(lambda: app.status == 'active')
+    unit = app.units[0]
+    await model.block_until(lambda: unit.agent_status == 'idle')
 
 
 async def test_haproxy_status(model):
@@ -97,10 +94,3 @@ async def test_add_reverse_proxy(model, app):
     await app.add_relation('reverseproxy', 'haproxy:reverseproxy')
     await model.block_until(lambda: haproxy.status == 'maintenance')
     await model.block_until(lambda: haproxy.status == 'active')
-
-
-# async def test_example_action(units):
-#     for unit in units:
-#         action = await unit.run_action('example-action')
-#         action = await action.wait()
-#         assert action.status == 'completed'
